45a09f040515f066c01e03d4bede97a21e09b26c
Corbin <RavetcoFX@gmail.com>
2014-03-10 14:24:21 -0600

    Merged from other pull requests and other work

--- files/usr/lib/cinnamon-settings/modules/cs_fonts.py
+++ files/usr/lib/cinnamon-settings/modules/cs_fonts.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from SettingsWidgets import *
+from gi.repository.Gtk import SizeGroup, SizeGroupMode
+
 
 class Module:
     def __init__(self, content_box):
@@ -11,7 +13,23 @@ class Module:
         self.name = "fonts"
         self.category = "appear"
         self.comment = _("Configure system fonts")
-        sidePage.add_widget(GSettingsFontButton(_("Default font"), "org.cinnamon.desktop.interface", "font-name", None))
+        
+        #Main Header Text
+        title = Gtk.Label()
+        title.set_markup("<span font_desc='10.5'><b>%s</b></span>" %_("Configure how your system fonts appear"))
+        sidePage.add_widget(title)
+        
+        #Some info about the settings
+        info = Gtk.Label(_("Fonts can make reading text easier, help save screen space, or simply personalize your setup."))
+        sidePage.add_widget(info)
+        
+        sidePage.add_widget(Gtk.Separator.new(Gtk.Orientation.HORIZONTAL))
+        
+        size_groups = getattr(self, "size_groups", [SizeGroup(SizeGroupMode.HORIZONTAL) for x in range(2)])
+        sidePage.add_widget(size_groups)
+        
+        size_groups[0].add_widget(Gtk.Label(_("Default font")))
+        sidePage.add_widget(GSettingsFontButton("", "org.cinnamon.desktop.interface", "font-name", None))
         sidePage.add_widget(GSettingsFontButton(_("Document font"), "org.gnome.desktop.interface", "document-font-name", None))
         sidePage.add_widget(GSettingsFontButton(_("Monospace font"), "org.gnome.desktop.interface", "monospace-font-name", None))
         sidePage.add_widget(GSettingsFontButton(_("Window title font"), "org.cinnamon.desktop.wm.preferences", "titlebar-font", None))
